07b146586c763969da7b94a1ecd26f5823c8c314
Victor Ocanto <vdiquez@gmail.com>
2021-09-17 17:45:47 -0500

    Adicion de solicitud de Datos

--- src/poo/MainClass.java
+++ src/poo/MainClass.java
@@ -21,7 +21,7 @@ public class MainClass {
         tuna.calculateTax();
 
         if (tuna.canItBeSold()) {
-            System.out.println("Nombre del Item: " + tuna.name);
+            System.out.println("Nombre del Item : " + tuna.name);
             System.out.println("Precio del Item: " + tuna.price);
             System.out.println("Impuesto del Item: " + tuna.tax);
             System.out.println("Total a pagar por el Item: " + tuna.getTotalToPay());
